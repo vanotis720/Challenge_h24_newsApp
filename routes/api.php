dd6e5edcae06293e74459d8fa58b3930b3aa2af4
vanotis720 <vanotis720@gmail.com>
2021-03-08 02:11:03 +0100

    added getUser profile and worked

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', [AuthController::class, 'getUser']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
 });
